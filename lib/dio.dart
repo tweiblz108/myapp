edd68f1eb16903da803c996b07d62eab405a78d4
NER0 <ner0@outlook.com>
2019-11-12 20:23:02 +0800

    修复请求封装中的错误

--- lib/dio.dart
+++ lib/dio.dart
@@ -13,19 +13,16 @@ Dio dio = (() {
 
   String hash(String str) => md5.convert(utf8.encode(str)).toString();
 
-  _dio.options.baseUrl = 'https://api.nerdlinux.com/delivery_v1';
+  _dio.options.baseUrl = 'https://api.nerdlinux.com/deliverer_v1';
 
   _dio.interceptors
-    ..add(LogInterceptor(responseBody: true, responseHeader: true))
     ..add(alice.getDioInterceptor())
     ..add(InterceptorsWrapper(onRequest: (RequestOptions options) async {
-      var timestamp = DateTime.now().microsecondsSinceEpoch ~/ 1000;
+      var timestamp = DateTime.now().millisecondsSinceEpoch ~/ 1000;
       var token = hash('$mobile$timestamp${hash(password)}');
 
       options.headers
           .addAll({'mobile': mobile, 'timestamp': timestamp, 'token': token});
-
-      print('$mobile, $timestamp, $token');
 
       return options;
     }));

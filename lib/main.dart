2692fdeae222e661fd12f25c39fd6255f5770329
NER0 <ner0@outlook.com>
2019-11-13 00:47:33 +0800

    加入基本的首页订单卡片样式

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dio.dart';
 
 // flutter padding 不计入 height 中
 
-
 const bold24Rotobo = TextStyle(
     fontFamily: 'Roboto',
     fontSize: 17,
@@ -37,11 +36,12 @@ class Header extends StatelessWidget {
           ),
           Container(child: action, width: 50)
         ],
-      ),      
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border(bottom: BorderSide(width: 1, color: Color.fromRGBO(204, 204, 204, 0.4)))
       ),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+              bottom: BorderSide(
+                  width: 1, color: Color.fromRGBO(204, 204, 204, 0.4)))),
       height: 50 + mediaQuery.viewPadding.top,
       padding: EdgeInsets.only(top: mediaQuery.viewPadding.top),
     );
@@ -115,20 +115,126 @@ class TabButton extends StatelessWidget {
   }
 }
 
+class OrderCard extends StatelessWidget {
+  final Map<String, dynamic> order;
+
+  OrderCard({this.order});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      padding: EdgeInsets.only(left: 15, right: 15),
+      child: Container(
+        padding: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  color: Color.fromRGBO(212, 226, 244, 0.58),
+                  offset: Offset(0, 2),
+                  blurRadius: 10,
+                  spreadRadius: 0)
+            ]),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text(
+                  '派送中',
+                  style: TextStyle(
+                      color: Color(0xFF1cb9b6),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: Text('订单 ${order['sn']}',
+                        style: TextStyle(fontSize: 15)),
+                  ),
+                ),
+                Text(
+                  '${order['shipping_distance']} km',
+                  style: TextStyle(fontSize: 15, color: Color(0xFF4083ec)),
+                )
+              ],
+            ),
+            Container(height: 15),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Icon(
+                    Icons.store,
+                    color: Color(0xff09b7fb),
+                  ),
+                  margin: EdgeInsets.only(right: 10),
+                ),
+                Expanded(child:  Text(
+                  order['restaurant']['name'],
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  softWrap: true,
+                ),)
+              ],
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Icon(
+                    Icons.face,
+                    color: Color(0xfff5a623),
+                  ),
+                  margin: EdgeInsets.only(right: 10),
+                ),
+                Expanded(
+                  child: Text(
+                    order['address'],
+                    style: TextStyle(fontSize: 18),
+                    softWrap: true,
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Text('下单时间 ${order['created_at']}'),
+                Text('|'),
+                Text(
+                    '送达时间 ${order['appointed_at']} - ${order['appointed_end']}')
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class IndexPageState extends State<IndexPage> {
-  List<Map<String, dynamic>> _orders = [];
+  List<dynamic> orders = [];
 
   void query() async {
     var response = await dio.get('/orders', queryParameters: {
       'filter': {
         'except_statuses': [9, -1, -2]
       },
-      'pageinfo': {
-        'limit': 100
-      }
+      'pageinfo': {'limit': 100}
     });
 
-    print(response.data);
+    setState(() {
+      orders = response.data;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    query();
   }
 
   @override
@@ -137,11 +243,21 @@ class IndexPageState extends State<IndexPage> {
         child: Column(
           children: <Widget>[
             Header(
-              action: GestureDetector(child: Icon(Icons.refresh), onTap: query,),
+              action: GestureDetector(
+                child: Icon(Icons.refresh),
+                onTap: query,
+              ),
               title: '订单',
             ),
             Expanded(
-              child: Text('Wang Yang'),
+              child: ListView.separated(
+                  itemBuilder: (context, i) {
+                    return OrderCard(order: orders[i]);
+                  },
+                  separatorBuilder: (context, i) {
+                    return Container(height: 15);
+                  },
+                  itemCount: orders.length),
             ),
             TabBar()
           ],
